3598e78c369984dd8d33b1964883e5320162dcdd
Adi Kurniawan <daimagine@gmail.com>
2015-07-14 10:00:51 +0700

    update some and add affiliate fields

--- models/customer.py
+++ models/customer.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, BigInteger
+from sqlalchemy import Column, String, BigInteger, DateTime
 from sqlalchemy.schema import Sequence
 from base import Base
 
@@ -9,8 +9,15 @@ class Customer(Base):
 	id = Column(BigInteger, Sequence('seq_customer_id', start=1, increment=1), primary_key=True)
 	name = Column(String(255), nullable=False)
 	email = Column(String(255), nullable=False)
+	password_1 = Column(String(1024))
+	client_token = Column(String(1024))
+	client_token_valid_time = Column(DateTime(timezone=False))
 
 
 class CustomerSchema(Schema):
 	class Meta:
 		fields = ('id', 'name', 'email')
+
+class CustomerAuthSchema(Schema):
+	class Meta:
+		fields = ('id', 'name', 'email', 'client_token', 'client_token_valid_time')	
--- models/product.py
+++ models/product.py
@@ -1,9 +1,12 @@
 
-from sqlalchemy import Column, String, BigInteger, Numeric, Boolean
+from sqlalchemy import Column, String, BigInteger, Numeric, Boolean, ForeignKey
 from sqlalchemy.schema import Sequence
+from sqlalchemy.orm import relationship, backref
 from base import Base
 
-from marshmallow import Schema
+from marshmallow import Schema, fields
+
+from customer import Customer, CustomerSchema
 
 
 class Product(Base):
@@ -11,21 +14,34 @@ class Product(Base):
     id = Column(BigInteger,
                 Sequence('seq_product_id', start=1, increment=1),
                 primary_key=True)
+    # customer relationship
+    customer_id = Column(BigInteger, ForeignKey('customer.id'))
+    customer = relationship("Customer", backref="products")
+    # fields
     name = Column(String(255), nullable=False)
-
     is_affiliate_ready = Column(Boolean, default=False)
     affiliate_percentage = Column(Numeric(3, 2))
     affiliate_fee = Column(Numeric(12, 2))
     affiliate_fee_type = Column(String(1))
+    price = Column(Numeric(12, 2))
+    image = Column(String(1024))
+    description = Column(String(1024))
+    headline = Column(String(1024))
 
 
 class ProductSchema(Schema):
+    customer = fields.Nested(CustomerSchema)
     class Meta:
         fields = (
             'id',
             'name',
+            'price',
             'is_affiliate_ready',
             'affiliate_percentage',
             'affiliate_fee',
-            'affiliate_fee_type'
+            'affiliate_fee_type',
+            'customer',
+            'image',
+            'description',
+            'headline'
         )
